4b9192964c068bb046596e471a409de4177142c1
Paul Mourer <paul.mourer@gmail.com>
2019-02-01 13:57:24 -0700

    playlists: all, by id

--- routes/api.js
+++ routes/api.js
@@ -10,5 +10,22 @@ router.get('/users/:uid',(req,res) => {
     res.send(user)
   })
 })
+.get('/users/:uid/playlists/', (req,res) =>{
+  const uid = req.params.uid
+  knex('users').where({spotify_id:uid}).first().then(user => {
+    knex('playlists').where({user_id:user.id}).then(playlists => {
+      res.send(playlists)
+    })
+  })
+})
+.get('/users/:uid/playlists/:pid', (req,res) =>{
+  const uid = req.params.uid
+  const pid = req.params.pid
+  knex('users').where({spotify_id:uid}).first().then(user => {
+    knex('playlists').where({user_id:user.id, spotify_playlist_id:pid}).first().then(playlist => {
+      res.send(playlist)
+    })
+  })
+})
 
 module.exports = router
