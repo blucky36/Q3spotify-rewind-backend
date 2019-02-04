024f9ea71fb85c3eb16f99221c5f30a3a05b2e26
Paul Mourer <paul.mourer@gmail.com>
2019-02-04 16:24:37 -0700

    added error for backing up with an existing playlist

--- routes/api.js
+++ routes/api.js
@@ -26,14 +26,16 @@ router.get('/users', (req, res) => {
 
   .get('/users/:uid/playlists/', (req, res) => {
     const uid = req.params.uid
+    console.log(uid)
     knex('users').where({ spotify_id: uid }).first().then(user => {
+      console.log(user)
       knex('playlists').where({ user_id: user.id }).then(playlists => {
         res.send(playlists)
       })
     })
   })
 
-  .post("/users/:uid/playlists", (req, res) => {
+  .post("/users/:uid/playlists", (req, res, next) => {
     const spotify_id = req.params.uid
     const {
       spotify_playlist_id,
@@ -43,22 +45,26 @@ router.get('/users', (req, res) => {
     } = req.body
     console.log("here");
     if (!spotify_playlist_id || !name || !spotify_id || !notes || !trackArray) return
-    console.log("passes error handling");
-    knex('users').where({ spotify_id }).first().then(user => {
-      console.log("inserts user");
-      return knex("playlists").insert({ spotify_playlist_id, name, user_id: user.id }).returning("*").then(playlist => {
-        console.log("inserts playlist");
-        return knex("versions").insert({ playlist_id: playlist[0].id, notes }).returning("*").then(version => {
-          console.log("inserts versions");
-          trackArray.forEach(track => {
-            knex("tracks").insert({ spotify_uri: track.track.uri, name: track.track.name, artist: track.track.artists[0].name, spotify_id: track.track.id }).returning("*").then(t => {
-              knex("versions_tracks").insert({ version_id: version[0].id, track_id: t[0].id }).returning("*").then(data => {})
+    knex('playlists').where({ spotify_playlist_id }).then(playlists => {
+      if (playlists.length > 0) throw new Error('playlist exists')
+    }).then(() => {
+      console.log("passes error handling");
+      knex('users').where({ spotify_id }).first().then(user => {
+        console.log("inserts user");
+        return knex("playlists").insert({ spotify_playlist_id, name, user_id: user.id }).returning("*").then(playlist => {
+          console.log("inserts playlist");
+          return knex("versions").insert({ playlist_id: playlist[0].id, notes }).returning("*").then(version => {
+            console.log("inserts versions");
+            trackArray.forEach(track => {
+              knex("tracks").insert({ spotify_uri: track.track.uri, name: track.track.name, artist: track.track.artists[0].name, spotify_id: track.track.id }).returning("*").then(t => {
+                knex("versions_tracks").insert({ version_id: version[0].id, track_id: t[0].id }).returning("*").then(data => {})
+              })
             })
+            return version[0]
           })
-          return version[0]
         })
       })
-    }).then(ver => res.status(201).send(ver))
+    }).catch(next).then(ver => res.status(201).send(ver))
   })
 
   .get('/users/:uid/playlists/:pid', (req, res) => {
@@ -123,26 +129,31 @@ router.get('/users', (req, res) => {
   })
 
   .get("/users/:uid/all", (req, res) => {
-  let tracksObj = {}
-  return knex("users").where("users.spotify_id", req.params.uid).first().then(user => {
-    return knex("playlists").where({ "playlists.user_id": user.id }).then(pArr => {
-      let pArrIds = pArr.map(p => p.id)
-      return knex("versions").whereIn("playlist_id", pArrIds).then(verArr => {
-        return Promise.all([...verArr.map((v) => {
-          return knex("versions_tracks").where({ "versions_tracks.version_id": v.id })
-            .innerJoin("tracks", "versions_tracks.track_id", "tracks.id").then(trackArr => {
-              return [trackArr,v.id]
-            })
-        }, {})]).then(tracks => ({ tracks:tracks.reduce((accum,t,i) => {
-          accum[t[1]] = t[0]
-          return accum
-        },{}), verArr, pArr, user }))
+    let tracksObj = {}
+    return knex("users").where("users.spotify_id", req.params.uid).first().then(user => {
+      return knex("playlists").where({ "playlists.user_id": user.id }).then(pArr => {
+        let pArrIds = pArr.map(p => p.id)
+        return knex("versions").whereIn("playlist_id", pArrIds).then(verArr => {
+          return Promise.all([...verArr.map((v) => {
+            return knex("versions_tracks").where({ "versions_tracks.version_id": v.id })
+              .innerJoin("tracks", "versions_tracks.track_id", "tracks.id").then(trackArr => {
+                return [trackArr, v.id]
+              })
+          }, {})]).then(tracks => ({
+            tracks: tracks.reduce((accum, t, i) => {
+              accum[t[1]] = t[0]
+              return accum
+            }, {}),
+            verArr,
+            pArr,
+            user
+          }))
+        })
       })
+    }).then(oi => {
+      res.status(200).send({ ...oi })
     })
-  }).then(oi => {
-    res.status(200).send({ ...oi })
   })
-})
 
   .delete('/users/:uid/playlists/:pid/versions/:vid', (req, res) => {
     const vid = req.params.vid
@@ -161,12 +172,17 @@ router.get('/users', (req, res) => {
           return Promise.all([...verArr.map((v) => {
             return knex("versions_tracks").where({ "versions_tracks.version_id": v.id })
               .innerJoin("tracks", "versions_tracks.track_id", "tracks.id").then(trackArr => {
-                return [trackArr,v.id]
+                return [trackArr, v.id]
               })
-          }, {})]).then(tracks => ({ tracks:tracks.reduce((accum,t,i) => {
-            accum[t[1]] = t[0]
-            return accum
-          },{}), verArr, pArr, user }))
+          }, {})]).then(tracks => ({
+            tracks: tracks.reduce((accum, t, i) => {
+              accum[t[1]] = t[0]
+              return accum
+            }, {}),
+            verArr,
+            pArr,
+            user
+          }))
         })
       })
     }).then(oi => {
