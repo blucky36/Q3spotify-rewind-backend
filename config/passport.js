752a3243f3374ef1116ee0a24a19e8af6d2302b7
Paul Mourer <paul.mourer@gmail.com>
2019-02-07 15:22:27 -0700

    typo

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const passport = require('passport')
 const SpotifyStrategy = require('passport-spotify').Strategy
 require('dotenv').config()
 
-const callbackURL = process.env.NODE_ENV === 'production' ? `${process.env.SERVER_URL}/auth/callback` : 'http://localhost:3000/auth/callback'
+const callbackURL = process.env.NODE_ENV === 'production' ? `${process.env.SERVER_URL}/auth/spotify/callback` : 'http://localhost:3000/auth/callback'
 
 passport.serializeUser((user, done) => {
   done(null, user)
